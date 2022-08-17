e62b33f8f129d2a761a88404542a3dd33e5dc30b
Novita Eka Widyastuti <novitaekawidyastuti@gmail.com>
2022-08-17 18:54:02 +0700

    sign up page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import SignInView from "./features/SignIn/SignInView";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SignUpView from "./features/SignUp/SignUpView";
 
 function App() {
   return (
     <div>
-      {/* <SignInView/> */}
       <SignUpView/>
     </div>
   );
