3dd5f1a09b86df2cd4963e8077ee877d53a45cf8
Yurham Afif <yurhamafif.edu@gmail.com>
2022-08-19 00:40:07 +0700

    [delete] remove profile card component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import AccountCard from "./shared/components/AccountCard/AccountCard";
 import EditProfileCard from "./shared/components/EditProfieCard/EditProfieCard";
-import ProfileCard from "./shared/components/ProfileCard/ProfileCard";
 
 function App() {
   return (
     <>
       <div>
-        <ProfileCard />
         <AccountCard />
       </div>
       <EditProfileCard />
