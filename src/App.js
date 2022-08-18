b9d27c11e3c58fda3973c0a4b1c36d4285c18fb2
Yurham Afif <yurhamafif.edu@gmail.com>
2022-08-18 16:23:49 +0700

    [delete] delete unused files in branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import PurchasedCard from "./shared/components/PurchasedCard/PurchasedCard";
-import ServiceCard from "./shared/components/ServiceCard/ServiceCard";
 
 function App() {
   return (
     <>
-      {/* <ServiceCard /> */}
       <PurchasedCard />
     </>
   );
