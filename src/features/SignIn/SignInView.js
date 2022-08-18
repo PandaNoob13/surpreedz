085dd03a7b785dbdfb360da19d8cea79f6f57cab
Novita Eka Widyastuti <novitaekawidyastuti@gmail.com>
2022-08-19 06:37:02 +0700

    edit after review 1 sign in page

--- src/features/SignIn/SignInView.js
+++ src/features/SignIn/SignInView.js
@@ -1,52 +1,50 @@
 import "./signInView.css"
 
 const SignInView = () => {
-  return (
-    <div className="col-md-12 signincontainer">
-          <div className="col-md-3 textsignin">
-              <div className="welcometext">
-                    Welcome Back !
-              </div>
-              <div className="line">
-              </div>
-              <div className="signexpression">
-                    Sign in to liberate our expression
+
+return (
+          <div className="bg-request-page">
+          <div className="container">
+              <div className="col-md-12 d-flex flex-row justify-content-center align-items-center">
+                  <div className="col-md-4 align-self-center">
+                      <div className="welcometext m-2">Welcome Back !</div>
+                      <div className="col-md-6 hr m-2"></div>
+                      <div className="liberate m-2">Sign in to liberate our expression</div>
+                  </div>
+
+                  <div className="col-md-4"></div>
+
+                  <div className="col-md-4 card align-items-center align-self-center" style={{backgroundColor:'#373535', borderRadius:'8px'}}>
+
+                      <form className="col-md-10 d-flex p-4 flex-column justify-content-center align-items-center text-center ">
+                          <input className="form-control p-3 m-2" placeholder="Username/email" />
+                          <input className="form-control p-3 m-2" placeholder="Password" />
+                          <button className="col-md-10 btn btn-light"
+                          >Continue</button>
+                          <div className="col-md-12 d-flex m-3 flex-row justify-content-around">
+                                <div class="form-check">
+                                        <input className="form-check-input" type="checkbox" value="" id="check-remmeber-me" />
+                                        <label className="already" for="check-remmeber-me">
+                                            Remember me
+                                        </label>
+                                </div>
+                                <div className="sign-in btn-link">
+                                Forget Password?
+                                </div>
+                          </div>
+                          
+                          <div className="col-md-12" style={{border: "1px solid #000000"}}></div>
+                          <div className="col-md-12 p-3 d-flex flex-row justify-content-center">
+                              <div className="already"> Not a member yet ?</div>
+                              <div className="sign-in btn-link"> Sign Up</div>
+                          </div>
+                      </form> 
+
+                  </div>
               </div>
           </div>
-
-          <div className= "signincard">
-              <div >
-                    <input className="input-box" style={{marginTop:"30px"}} placeholder="Username/email"/>
-              </div>
-              <div>
-                    <input className="input-box" placeholder="Password"/>
-              </div>
-              <div>
-                    <button className="button-continue">Continue</button>
-              </div>
-
-              <div className="remember-password">
-                  <div style={{display:'flex'}}>
-                      <input className="checkbox" type="checkbox" />
-                     
-                      <label className="label-remember">Remember me</label>
-                  </div>
-                  <div className="forget-password">
-                        Forget Password?
-                  </div>
-              </div>
-
-              <div>
-                <hr className="line-2"></hr>
-              </div>
-              <div className="signup">
-                  <div style={{color:'#d9d9d9', fontWeight:'100'}}>Not a member yet ? </div>
-                  <div style={{color:'#ffffff', fontWeight:'bold' ,marginLeft:'5px'}}>Sign up </div>
-              </div>
-          </div>
-          
     </div>
-  )
+);
 }
 
 export default SignInView;
