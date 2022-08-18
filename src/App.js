40301c25887bc38906a36a73909998c55e74517a
Yurham Afif <yurhamafif.edu@gmail.com>
2022-08-18 16:27:37 +0700

    [delete] delete unused files in branch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import AccountCard from "./shared/components/AccountCard/AccountCard";
 import EditProfileCard from "./shared/components/EditProfieCard/EditProfieCard";
 import ProfileCard from "./shared/components/ProfileCard/ProfileCard";
-import PurchasedCard from "./shared/components/PurchasedCard/PurchasedCard";
-import ServiceCard from "./shared/components/ServiceCard/ServiceCard";
 
 function App() {
   return (
     <>
-      <ServiceCard />
-      <PurchasedCard />
       <div>
         <ProfileCard />
         <AccountCard />
