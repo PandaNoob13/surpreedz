fecb9cef7ad0f0a288e65f07adcaff10c2eb6236
Novita Eka Widyastuti <novitaekawidyastuti@gmail.com>
2022-08-18 21:27:07 +0700

    sign up page only

--- src/features/SignUp/SignUpView.js
+++ src/features/SignUp/SignUpView.js
@@ -2,6 +2,7 @@ import { useState } from "react"
 import "./signUpView.css"
 
 const SignUpView = () => {
+
   const [step,setStep]= useState(1);
 
   const handleContinue = () => {
@@ -12,93 +13,95 @@ const SignUpView = () => {
     setStep(step - 1);
   }
 
-
   switch (step) {
     case 1:
-      return (
-        <div className="col-md-12 signupcontainer">
-              <div className="col-md-4 textsignup">
-                  <div className="welcometext">
-                        Welcome to Surpreedz
-                  </div>
-                  <div className="line">
-                  </div>
-                  <div className="signexpression">
-                        Sign up to liberate our expression
-                  </div>
-              </div>
+      return(
+        <div className="bg-request-page">
+              <div className="container">
+                <br/>
+                <br/>
+                <br/>
+                  <div className="col-md-12 d-flex align-items-center flex-row justify-content-around">
+                      <div className="col-md-4 align-self-center">
+                          <div className="welcometext m-2">Welcome to Surpreedz</div>
+                          <div className="col-md-6 hr m-2"></div>
+                          <div className="liberate m-2">Sign up to liberate out expressions</div>
+                      </div>
     
-             
-                    <div className= "signupcard">
-                    <div >
-                          <input className="input-box" style={{marginTop:"50px"}} placeholder="Enter your email"/>
-                    </div>
-                    <div>
-                          <button className="button-continue" onClick={handleContinue}>Continue</button>
-                    </div>
-      
-                    <div className="by-joining">                    
-                      By joining I agree t receive emails from Surpreedz
-                    </div>
-      
-                    <div>
-                      <hr className="line-2"></hr>
-                    </div>
-                    <div className="signin">
-                        <div style={{color:'#d9d9d9', fontWeight:'100'}}>Already a member ? </div>
-                        <div style={{color:'#ffffff', fontWeight:'bold' ,marginLeft:'5px'}}>Sign in </div>
-                    </div>
-                </div>
+                      <div className="col-md-4 card bg-card align-self-center align-items-center">
+                          <form className="col-md-10 d-flex p-4 flex-column justify-content-center align-items-center text-center ">
+                              <input className="form-control p-3 m-4" placeholder="Enter Your Email" />
+                              <button className="col-md-5 btn btn-light"
+                              onClick={handleContinue}
+                              >Continue</button>
+                              <div className="by-joining m-4">By joining I agree to receive emails from Surpreedz</div>
+                              
+                              <div className="col-md-12" style={{border: "1px solid #000000"}}></div>
+                              <div className="col-md-12 p-3 d-flex flex-row justify-content-center">
+                                  <div className="already"> Already a member ?</div>
+                                  <div className="sign-in btn-link"> Sign In</div>
+                              </div>
+                          </form> 
+                      </div>
+                  </div>
+                  <br/>
+                  <br/>
+                  <br/>
+    
+              </div>
         </div>
       );
-    case 2:
-      return (
-        <div className="col-md-12 signupcontainer">
-              <div className="col-md-4 textsignup">
-                  <div className="welcometext">
-                        Welcome to Surpreedz
+      case 2:
+        return (
+          <div className="bg-request-page">
+          <div className="container">
+            <br/>
+            <br/>
+            <br/>
+              <div className="col-md-12 d-flex align-items-center flex-row justify-content-around">
+                  <div className="col-md-4 align-self-center">
+                      <div className="welcometext m-2">Welcome to Surpreedz</div>
+                      <div className="col-md-6 hr m-2"></div>
+                      <div className="liberate m-2">Sign up to liberate out expressions</div>
                   </div>
-                  <div className="line">
-                  </div>
-                  <div className="signexpression">
-                        Sign up to liberate our expression
-                  </div>
-              </div>
-            
-              <div className= "signupcard">
-                  <div >
-                        <input className="input-box" style={{marginTop:"30px"}} placeholder="Choose a Username"/>
-                  </div>
-                  <div>
-                        <input className="input-box" placeholder="Choose a Password"/>
-                  </div>
-                  <div style={{display:'flex'}}>
-                        <button className="button-back" onClick={handleBack}>Back</button>
-                        <button className="button-join">Join</button>
-                  </div>
-    
-                  <div className="by-joining">                    
-                    By joining, you agree to Surpreedz's Terms of Service, as well as to receive occasional emails from us.
-                  </div>
-    
-                  <div>
-                    <hr className="line-2"></hr>
-                  </div>
-                  <div className="signin">
-                      <div style={{color:'#d9d9d9', fontWeight:'100'}}>Already a member ? </div>
-                      <div style={{color:'#ffffff', fontWeight:'bold', marginLeft:'5px'}}>Sign in </div>
-                  </div>
-              </div>
-    
-    
-        </div>
-      );
-  
-    default:
-      break;
-  }
 
- 
+                  <div className="col-md-4 card bg-card align-self-center align-items-center">
+                      <form className="col-md-10 d-flex p-4 flex-column justify-content-center align-items-center text-center ">
+                          <input className="form-control p-3 m-2" placeholder="Choose a Username" />
+                          <input className="form-control p-3 m-2" placeholder="Choose a Password" />
+
+                          <div className="col-md-12 d-flex flex-row justify-content-around m-3">
+                          <button className="col-md-5 btn btn-light"
+                          onClick={handleBack}
+                          >Back</button>
+
+                          <div className="col-md-2"></div>
+
+                          <button className="col-md-5 btn btn-light"
+                          >Join</button>
+
+                          </div>
+                          
+                          <div className="by-joining m-2">By joining, you agree to Surpreedz's Terms of Service, as well as to receive occasional emails from us</div>
+                          
+                          <div className="col-md-12" style={{border: "1px solid #000000"}}></div>
+                          <div className="col-md-12 p-3 d-flex flex-row justify-content-center">
+                              <div className="already"> Already a member ?</div>
+                              <div className="sign-in btn-link"> Sign In</div>
+                          </div>
+                      </form> 
+                  </div>
+              </div>
+              <br/>
+              <br/>
+              <br/>
+
+          </div>
+    </div>
+        );
+      default:
+         break;
+  }
 }
 
 export default SignUpView;
