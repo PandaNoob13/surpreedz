2b177c09737cfa4df8ee05886cc6e68a2d80b28a
Novita Eka Widyastuti <novitaekawidyastuti@gmail.com>
2022-08-19 18:02:54 +0700

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import './App.css';
 import NavbarLogin from './shared/components/navbar/NavbarLogin';
-import NavbarSellerBuyer from './shared/components/navbar/NavbarSellerBuyer';
 import NavbarSeller from './shared/components/navbar/NavbarSeller';
 import NavbarBuyer from './shared/components/navbar/NavbarBuyer';
 
 function App() {
   return (
     <div className="App">
-      {/* Navbar login
-     <NavbarLogin/> */}
+      Navbar login
+     <NavbarLogin/>
+     
      <br/>
-     <NavbarSellerBuyer/>
+     Navbar seller
+     <NavbarSeller/>
      <br/>
-     {/* Navbar seller
-     <NavbarSeller/> */}
-     <br/>
-     {/* Navbar buyer
-     <NavbarBuyer/> */}
+     Navbar buyer
+     <NavbarBuyer/>
      <br/>
     </div>
     
