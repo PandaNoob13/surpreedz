55cfdf341c7cf65055c40b49019ff78542c554ed
Yurham Afif <yurhamafif.edu@gmail.com>
2022-08-20 16:57:48 +0700

    [add] create edit profile page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
+import EditProfilePage from "./features/EditProfilePage/EditProfilePage";
 import AccountCard from "./shared/components/AccountCard/AccountCard";
 import EditProfileCard from "./shared/components/EditProfieCard/EditProfieCard";
 
 function App() {
   return (
     <>
-      <div>
-        <AccountCard />
-      </div>
-      <EditProfileCard />
+      <EditProfilePage />
     </>
   );
 }
