8429ec9b2251669ebfdc87a05508028ffb6cb728
Novita Eka Widyastuti <novitaekawidyastuti@gmail.com>
2022-08-24 06:22:39 +0700

    change purchase confirmation page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import AppRouter from "./navigation/AppRouter";
-import PurchaseConfirmationPage from "./features/PurchaseConfirmationPage/PurchaseConfirmationPage"
 
 const App = () => {
   return (
     <>
-    {/* <AppRouter/> */}
-    <PurchaseConfirmationPage/>
+    <AppRouter/>
     
     </>
   )
