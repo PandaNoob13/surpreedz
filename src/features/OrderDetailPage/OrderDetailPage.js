c3e71e7ecc65731492ecaa0972109388a450fdab
Novita Eka Widyastuti <novitaekawidyastuti@gmail.com>
2022-08-23 19:50:24 +0700

    change app routing and routing order detail page

--- src/features/OrderDetailPage/OrderDetailPage.js
+++ src/features/OrderDetailPage/OrderDetailPage.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import OccasionCard from '../../shared/components/OccasionCard/OccasionCard'
 import PersonalisedMessageCard from '../../shared/components/PersonalisedMessageCard/PersonalisedMessageCard'
 import ProfileCard from '../../shared/components/ProfileCard/ProfileCard'
 import ReviewCard from '../../shared/components/ReviewCard/ReviewCard'
 import VideoCarouselCard from '../../shared/components/VideoCarouselCard/VideoCarouselCard'
+import {useAuth} from "../../shared/auth/UseAuth"
 
 function OrderDetailPage() {
+    const {token} = useAuth();
+    
+
     return (
         <div className='text-white' style={{background: "#212121", marginTop: '50px'}}>
             <div className='container py-5'>
@@ -21,7 +25,10 @@ function OrderDetailPage() {
                         <OccasionCard />
                         <PersonalisedMessageCard />
                         <div className='d-flex justify-content-end'>
-                            <NavLink to='/purchase-confirmation' className="btn btn-light btn-lg mt-3" role="button">Send Request</NavLink>
+                            { token ? <NavLink to='/purchase-confirmation' className="btn btn-light btn-lg mt-3" role="button">Send Request</NavLink>
+                            :
+                            <NavLink to='/sign-in' className="btn btn-light btn-lg mt-3" role="button">Send Request</NavLink>
+                        }
                         </div>
                     </div>
                 </div>
