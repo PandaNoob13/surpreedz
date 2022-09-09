5da254d5fd04c05841a1d2ae6510e359d7f32d85
Novita Eka Widyastuti <novitaekawidyastuti@gmail.com>
2022-09-09 08:45:42 +0700

    setting redux

---
+++ src/redux/OrderDetail/OrderDetailReducer.js
@@ -0,0 +1,16 @@
+const orderInitialState = {
+
+}
+
+
+const orderDetailReducer = (state = orderInitialState, action) => {
+    switch(action.type){
+        default:
+            return state;
+    }
+  
+}
+
+export default orderDetailReducer;
+
+ 
---
+++ src/store.js
@@ -0,0 +1,12 @@
+import {combineReducers, createStore, compose,applyMiddleware } from "redux";
+import orderDetailReducer from "./redux/OrderDetail/OrderDetailReducer";
+import thunk from 'redux-thunk';
+
+
+const rootReducer = combineReducers({
+    orderDetailReducer
+})
+
+export const setupStore = () => {
+    return createStore(rootReducer, compose(applyMiddleware(thunk)));
+}
