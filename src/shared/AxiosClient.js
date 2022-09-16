c025393fc464abbd267e76fb798c38c74b392634
Samuel Maynard <samuelmaynard13@gmail.com>
2022-09-16 16:14:06 +0700

    Change env files

--- src/shared/AxiosClient.js
+++ src/shared/AxiosClient.js
@@ -4,5 +4,5 @@ import axios from "axios"
 export const clientInstance = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL
 }) 
-
+clientInstance.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 clientInstance.interceptors.request.use(AuthHeaderInterceptor)
